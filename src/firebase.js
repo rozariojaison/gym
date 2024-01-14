8d0ba2f933212ae88bdc8b6d1bd8da4ddab1ae3a
rozariojaison <rozariojaison@gmail.com>
2024-01-15 01:56:34 +0530

    21

--- src/firebase.js
+++ src/firebase.js
@@ -4,11 +4,6 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyArqUN08p2tDoIZctkZlTudnKI2pSYMLtg",
   authDomain: "nextlevelfitnessgy.firebaseapp.com",
