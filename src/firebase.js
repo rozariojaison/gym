dac63cdcc45b79ffaa0cdf0df3f161b2e34f5458
rozariojaison <rozariojaison@gmail.com>
2024-01-14 18:20:12 +0530

    15

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAui9fjs71w8y8MQX-w0qYtxGyaOGQquBs",
-  authDomain: "gymate-gym.firebaseapp.com",
-  projectId: "gymate-gym",
-  storageBucket: "gymate-gym.appspot.com",
-  messagingSenderId: "1086911363831",
-  appId: "1:1086911363831:web:8dac101ceb861e5c71b1f2",
+  apiKey: "AIzaSyArqUN08p2tDoIZctkZlTudnKI2pSYMLtg",
+  authDomain: "nextlevelfitnessgy.firebaseapp.com",
+  databaseURL: "https://nextlevelfitnessgy-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nextlevelfitnessgy",
+  storageBucket: "nextlevelfitnessgy.appspot.com",
+  messagingSenderId: "76843423139",
+  appId: "1:76843423139:web:b481256ad49e479de77616",
+  measurementId: "G-T364YDX0VN"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
